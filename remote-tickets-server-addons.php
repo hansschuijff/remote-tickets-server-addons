a047d280714f4a0a4b00ad52eec9f3eda34db824
Hans Schuijff <hans@dewitteprins.nl>
2026-08-16 22:32:18 +0200

    DocBlock aangevuld met versie 0.1.0 als eerste werkende release

--- remote-tickets-server-addons.php
+++ remote-tickets-server-addons.php
@@ -1,11 +1,31 @@
 <?php
 /**
- * Plugin Name: Remote Tickets Server Add-ons
- * Description: Voegt wat extra gegevens toe aan de response van de TEC Rest API en maakt woocommerce geschikt om meerdere tickets via een url in de winkelmand te plaatsen.
- * Version:     0.3.0
- * Author:      De Witte Prins
- * License:     GPL2
- * Text Domain: remote-tickets-server-dwp
+ * Remote Tickets Server Add-ons
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Remote Tickets Server Add-ons
+ * Plugin URI:        https://github.com/hansschuijff/remote-tickets-server-addons
+ * Description:       Voegt wat extra gegevens toe aan de response van de The Events Calendar Rest API en maakt woocommerce geschikt om meerdere tickets via een url in de winkelmand te plaatsen.
+ * Version:           0.1.0
+ * Author:            Hans Schuijff @hansschuijff
+ * Author URI:        https://dewitteprins.nl
+ * Text Domain:       remote-tickets-server-dwp
+ * Domain Path:       /languages
+ * License:           GPL-2.0
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/hansschuijff/remote-tickets-server-addons
+ * GitHub Plugin URI: https://github.com/hansschuijff/remote-tickets-server-addons
+ * GitHub Branch:     master
+ * Requires WP:       7.0
+ * Tested up to:      7.4
+ * Requires PHP:      8.4
+ *
+ * @package           DeWittePrins\RemoteTicketsServerAddons
+ * @author            Hans Schuijff (@hansschuijff)
+ * @license           GPL-2.0
+ * @link              https://dewitteprins.nl
+ * @see               https://github.com/hansschuijff/remote-tickets-server-addons
+ * @license           GPL-2.0
  */
 
 namespace DeWittePrins\RemoteTicketsServerAddons;
